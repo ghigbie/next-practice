e3a92b04d99599d1c8a9302e26c0ae6079879644
ghigbie <gehigbie@gmail.com>
2019-09-24 18:25:58 -0700

    Add first react file

---
+++ first-next/.next/static/webpack/static/development/pages/index.js.4a4461c6c9abfae40484.hot-update.js
@@ -0,0 +1,33 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.jsx":
+/*!*************************!*\
+  !*** ./pages/index.jsx ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/georgehigbie/next/first-next/pages/index.jsx";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+var Index = function Index() {
+  return __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, "Yo!");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Index);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.4a4461c6c9abfae40484.hot-update.js.map
---
+++ first-next/pages/index.jsx
@@ -0,0 +1,7 @@
+const Index = () => (
+    <div>
+    Yo!
+    </div>
+);
+
+export default Index;
