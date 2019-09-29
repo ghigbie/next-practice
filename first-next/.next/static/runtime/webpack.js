0dea572bdb00a4b0777709446f3e5750954b0cfe
ghigbie <gehigbie@gmail.com>
2019-09-28 19:34:08 -0700

    Add server file

--- first-next/.next/static/runtime/webpack.js
+++ first-next/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2b69b43603513f3de858";
+/******/ 	var hotCurrentHash = "472c831b8b427b163652";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
